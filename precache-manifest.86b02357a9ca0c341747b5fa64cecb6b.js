bb2f5db53784d0da78738c01f3f4be6dc24f902e
leilei1214 <a0933252747@gmail.com>
2021-10-24 09:19:44 +0800

    Updates

---
+++ precache-manifest.86b02357a9ca0c341747b5fa64cecb6b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd33b031111790350dd89e2451ec7a49",
+    "url": "/li/index.html"
+  },
+  {
+    "revision": "22f9b02ea1b23bf94536",
+    "url": "/li/static/css/main.4408340d.chunk.css"
+  },
+  {
+    "revision": "5c6e95035751d763fe85",
+    "url": "/li/static/js/2.bcd8269a.chunk.js"
+  },
+  {
+    "revision": "22f9b02ea1b23bf94536",
+    "url": "/li/static/js/main.8c33e74e.chunk.js"
+  },
+  {
+    "revision": "77569e63909fd1b48354",
+    "url": "/li/static/js/runtime~main.03207684.js"
+  },
+  {
+    "revision": "1dc87bbd37b6d19da8e55da00a486d8d",
+    "url": "/li/static/media/part_75428_3940907_76710.1dc87bbd.png"
+  }
+]);
